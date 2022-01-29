f5a592edf8914cc31c72423de7d83c888feba7d3
VenkatRamanaReddyMarri <marrivenkatramanar@gmail.com>
2022-01-29 18:06:44 +0530

    added onboard screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:haimal/routes.dart';
+import 'package:haimal/screens/onboarding/onboarding_page.dart';
 import 'package:haimal/screens/profile/profile_screen.dart';
 import 'package:haimal/screens/splash/splash_screen.dart';
 import 'package:haimal/theme.dart';
@@ -18,7 +19,7 @@ class MyApp extends StatelessWidget {
       theme: theme(),
       // home: SplashScreen(),
       // We use routeName so that we dont need to remember the name
-      initialRoute: SplashScreen.routeName,
+      initialRoute: OnboardingPage.routeName,
       routes: routes,
     );
   }
--- lib/routes.dart
+++ lib/routes.dart
@@ -1,3 +1,5 @@
+
+
 import 'package:flutter/widgets.dart';
 import 'package:haimal/screens/cart/cart_screen.dart';
 import 'package:haimal/screens/complete_profile/complete_profile_screen.dart';
@@ -5,6 +7,7 @@ import 'package:haimal/screens/details/details_screen.dart';
 import 'package:haimal/screens/forgot_password/forgot_password_screen.dart';
 import 'package:haimal/screens/home/home_screen.dart';
 import 'package:haimal/screens/login_success/login_success_screen.dart';
+import 'package:haimal/screens/onboarding/onboarding_page.dart';
 import 'package:haimal/screens/otp/otp_screen.dart';
 import 'package:haimal/screens/profile/profile_screen.dart';
 import 'package:haimal/screens/sign_in/sign_in_screen.dart';
@@ -15,6 +18,7 @@ import 'screens/sign_up/sign_up_screen.dart';
 // We use name route
 // All our routes will be available here
 final Map<String, WidgetBuilder> routes = {
+  OnboardingPage.routeName: (context) => OnboardingPage(),
   SplashScreen.routeName: (context) => SplashScreen(),
   SignInScreen.routeName: (context) => SignInScreen(),
   ForgotPasswordScreen.routeName: (context) => ForgotPasswordScreen(),
